22b90ca3466b1b5b351bdd55b1a19d1cbe7857e2
liab <1816692643@qq.com>
2020-04-12 20:59:32 +0800

    对应到RemoteMySQL

--- src/main/java/com/test/dto/User.java
+++ src/main/java/com/test/dto/User.java
@@ -7,9 +7,12 @@ public class User {
     private String id;
     private String userName;
     private String password;
+    private String name;
     private Integer age;
+    private Integer sex;
     private Date birthday;
     private String created;
+    private String updated;
 
     public String getId() {
         return id;
@@ -35,12 +38,28 @@ public class User {
         this.password = password;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Integer getAge() {
         return age;
     }
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
     }
 
     public Date getBirthday() {
@@ -59,15 +78,26 @@ public class User {
         this.created = created;
     }
 
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id='" + id + '\'' +
                 ", userName='" + userName + '\'' +
                 ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
                 ", age=" + age +
+                ", sex=" + sex +
                 ", birthday=" + birthday +
                 ", created='" + created + '\'' +
+                ", updated='" + updated + '\'' +
                 '}';
     }
 }
